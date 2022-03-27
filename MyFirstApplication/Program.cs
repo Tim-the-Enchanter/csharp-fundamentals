e7ebc442c43f5a9a3b288c4c527f11fe386af10f
Tim Donnelley <sgtpixel@gmail.com>
2022-03-26 22:40:46 -0500

    CAC-103 Lesson1 Value Types

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -1,11 +1,11 @@
 ﻿using MyFirstApplication;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
 // The bleow statement calls a local function   
 // single line comment
-HouseExample();
+
 
 /*
  Multi-line comment
@@ -14,19 +14,36 @@ are inside of members and are private by default. This means
 They are specific to that member and can only be called
 inside that member.
  */
+
+// Lesson Examples
+// HouseExample();
+Lesson1Example();
+
 void HouseExample()
 {
     House myHouse = new House();
     myHouse.DoorOpenClose();
 }
 
+// Lesson 1 Value Types
+void Lesson1Example()
+{
+    Lesson1 myLesson = new Lesson1();
+    myLesson.IntMinMax();
+    myLesson.IntegralExample();
+    myLesson.ConvertFloatToInt();
+    myLesson.LongFromInt();
+}
+
+
 /*
  This local function is 
 for my first exercise lab.
 */
 
+MyLocalFunction();
 
-    void MyLocalFunction()
+void MyLocalFunction()
 {
     Console.WriteLine("This is my first sentence");
     Console.WriteLine("I am new to CSharp");
@@ -34,4 +51,4 @@ for my first exercise lab.
     Console.WriteLine("I am learning stuff every day.");
 }
 
-MyLocalFunction();
+
